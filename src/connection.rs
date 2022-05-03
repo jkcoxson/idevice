7cee2c699123d324a7bacbdc54bfb1651945d2f3
jkcoxson <jkcoxson@gmail.com>
2022-05-03 14:22:45 -0600

    Clean connection code

--- src/connection.rs
+++ src/connection.rs
@@ -1,17 +1,24 @@
 // jkcoxson
 
 use crate::muxer::DeviceProperties;
-use serde::{Deserialize, Serialize};
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::UnixStream,
 };
 
 pub struct Connection {
+    // Streams for writing and reading
     unix_stream: Option<UnixStream>,
     tcp_stream: Option<tokio::net::TcpStream>,
+
+    // Public properties
     pub properties: DeviceProperties,
-    pub service_type: String,
+    pub label: String,
+
+    // Private property caches
+    service_type: Option<String>,
+    product_version: Option<String>,
 }
 
 impl Connection {
@@ -35,73 +42,15 @@ impl Connection {
                 };
 
                 // Create a new TcpStream to the device
-                let mut stream = tokio::net::TcpStream::connect(socket_addr).await?;
-
-                // Query the device for the connection type
-                #[derive(Serialize)]
-                #[serde(rename_all = "PascalCase")]
-                struct Query {
-                    label: String,
-                    request: String,
-                }
-
-                let query = Query {
-                    label: label.into(),
-                    request: "QueryType".to_string(),
-                };
-
-                // Serialize the query to a plist
-                let mut to_send = Vec::new();
-                let _ = match plist::to_writer_xml(&mut to_send, &query) {
-                    Ok(_) => (),
-                    Err(e) => {
-                        return Err(std::io::Error::new(
-                            std::io::ErrorKind::Other,
-                            format!("Unable to serialize packet: {}", e),
-                        ));
-                    }
-                };
-
-                // Get the size of the packet and append it to the front
-                let size = to_send.len() as u32;
-                let mut size = size.to_be_bytes().to_vec();
-                size.extend_from_slice(&to_send);
-
-                // Send the packet to the device
-                stream.write_all(&size).await?;
-
-                // Read the len from the device
-                let mut buf = [0; 4];
-                let size = stream.read(&mut buf).await?;
-                let size_buf = &buf[..size];
-                let size = u32::from_be_bytes(size_buf.try_into().unwrap());
-
-                // Read the packet from the device
-                let mut buf = vec![0; size as usize];
-                let size = stream.read(&mut buf).await?;
-                let buf = &buf[..size];
-
-                // Deserialize the response
-                #[derive(Deserialize)]
-                #[serde(rename_all = "PascalCase")]
-                struct Res {
-                    type_: String,
-                }
-                let response: Res = match plist::from_bytes(buf) {
-                    Ok(v) => v,
-                    Err(e) => {
-                        return Err(std::io::Error::new(
-                            std::io::ErrorKind::Other,
-                            format!("Unable to deserialize packet: {}", e),
-                        ));
-                    }
-                };
+                let stream = tokio::net::TcpStream::connect(socket_addr).await?;
 
                 return Ok(Connection {
                     unix_stream: None,
                     tcp_stream: Some(stream),
                     properties: properties.clone(),
-                    service_type: response.type_,
+                    service_type: None,
+                    product_version: None,
+                    label: label.into(),
                 });
             }
             "USB" => {
@@ -112,6 +61,25 @@ impl Connection {
                 "Unknown connection type",
             )),
         }
+    }
+
+    pub async fn get_service_type(&mut self) -> Result<String, std::io::Error> {
+        if self.service_type.is_some() {
+            return Ok(self.service_type.clone().unwrap());
+        }
+        // Query the device for the connection type
+        let query = Query {
+            label: self.label.clone(),
+            request: "QueryType".to_string(),
+        };
+
+        self.write_plist(&query).await?;
+
+        let res: QueryRes = self.read_plist().await?;
+
+        self.service_type = Some(res.type_.clone());
+
+        Ok(res.type_)
     }
 
     pub(crate) async fn read(&mut self) -> Result<Vec<u8>, std::io::Error> {
@@ -141,6 +109,21 @@ impl Connection {
         };
     }
 
+    pub(crate) async fn write_plist(
+        &mut self,
+        plist: &impl Serialize,
+    ) -> Result<(), std::io::Error> {
+        let to_send = plist_to_binary(plist)?;
+        self.write(&to_send).await?;
+        Ok(())
+    }
+
+    pub(crate) async fn read_plist<T: DeserializeOwned>(&mut self) -> Result<T, std::io::Error> {
+        let bytes = self.read().await?;
+        let plist: T = binary_to_plist(&bytes)?;
+        return Ok(plist);
+    }
+
     pub(crate) async fn write(&mut self, data: &[u8]) -> Result<(), std::io::Error> {
         match self.unix_stream {
             Some(ref mut unix_stream) => {
@@ -159,4 +142,55 @@ impl Connection {
             }
         };
     }
+}
+
+pub(crate) fn plist_to_binary(plist: impl Serialize) -> Result<Vec<u8>, std::io::Error> {
+    let mut buf = Vec::new();
+    let _ = match plist::to_writer_xml(&mut buf, &plist) {
+        Ok(_) => (),
+        Err(e) => {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                format!("Unable to serialize packet: {}", e),
+            ));
+        }
+    };
+    Ok(buf)
+}
+
+pub(crate) fn binary_to_plist<'a, T: DeserializeOwned>(data: &[u8]) -> Result<T, std::io::Error> {
+    let response: T = match plist::from_bytes(data) {
+        Ok(v) => v,
+        Err(e) => {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                format!("Unable to deserialize packet: {}", e),
+            ));
+        }
+    };
+
+    Ok(response)
+}
+
+/// The initial packet sent to the device after connection
+#[derive(Serialize)]
+#[serde(rename_all = "PascalCase")]
+pub(crate) struct Query {
+    label: String,
+    request: String,
+}
+
+/// The response to the initial packet sent to the device after connection
+#[derive(Deserialize)]
+#[serde(rename_all = "PascalCase")]
+pub(crate) struct QueryRes {
+    type_: String,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+pub(crate) struct RequestKey {
+    label: String,
+    key: String,
+    request: String,
 }
