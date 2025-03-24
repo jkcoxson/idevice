7f93b664d40cbc8f0d9622d78f1ab3d2b994661f
Jackson Coxson <jkcoxson@gmail.com>
2025-03-24 15:30:39 -0600

    Automatically generate header with cbindgen

---
+++ ffi/build.rs
@@ -0,0 +1,18 @@
+// Jackson Coxson
+
+use std::env;
+
+fn main() {
+    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+
+    cbindgen::Builder::new()
+        .with_crate(crate_dir)
+        .with_header(
+            "// Jackson Coxson\n// Bindings to idevice - https://github.com/jkcoxson/idevice",
+        )
+        .with_language(cbindgen::Language::C)
+        .with_sys_include("sys/socket.h")
+        .generate()
+        .expect("Unable to generate bindings")
+        .write_to_file("idevice.h");
+}
