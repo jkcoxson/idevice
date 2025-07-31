8ee58c350210fe258489d1622183213e05e80bdb
Jackson Coxson <jkcoxson@gmail.com>
2025-07-31 11:14:42 -0600

    Add ring as optional crypto provider

--- idevice/src/lib.rs
+++ idevice/src/lib.rs
@@ -390,9 +390,16 @@ impl Idevice {
         pairing_file: &pairing_file::PairingFile,
     ) -> Result<(), IdeviceError> {
         if CryptoProvider::get_default().is_none() {
-            if let Err(e) =
-                CryptoProvider::install_default(rustls::crypto::aws_lc_rs::default_provider())
-            {
+            let crypto_provider = if cfg!(feature = "ring") {
+                debug!("Using ring crypto backend");
+                rustls::crypto::ring::default_provider()
+            } else if cfg!(feature = "aws-lc") {
+                debug!("Using aws-lc crypto backend");
+                rustls::crypto::aws_lc_rs::default_provider()
+            } else {
+                panic!("No crypto provider compiled in! Use one of the features for idevice to specify a provider");
+            };
+            if let Err(e) = CryptoProvider::install_default(crypto_provider) {
                 // For whatever reason, getting the default provider will return None on iOS at
                 // random. Installing the default provider a second time will return an error, so
                 // we will log it but not propogate it. An issue should be opened with rustls.
